fixed a bug in PGD multi-gpu, added some gradient masking checking code

cifar/script/check_cifar.py
@@ -36,8 +36,6 @@ model_dir = config['model_dir']
 cifar = cifar10_input.CIFAR10Data(data_path)
 
 model = Model(config, mode='eval')
-from pgd_attack import LinfPGDAttack
-attack = LinfPGDAttack(model, epsilon=8 / 255., num_steps=7, step_size=2 / 255., random_start=True, loss_func='xent')
 
 global_step = tf.contrib.framework.get_or_create_global_step()
 
@@ -48,22 +46,31 @@ eval_dir = os.path.join(model_dir, 'eval')
 if not os.path.exists(eval_dir):
   os.makedirs(eval_dir)
 
-last_checkpoint_filename = ''
 already_seen_state = False
 
 saver = tf.train.Saver()
 summary_writer = tf.summary.FileWriter(eval_dir)
 
+check_dict = {
+'nat_img': [],
+'adv_img': [],
+'nat_pred': [],
+'adv_pred': [],
+'nat_preds': [],
+'adv_preds': [],
+'label': [],
+'ckeck_idx': []
+}
+
 # A function for evaluating a single checkpoint
 def evaluate_checkpoint(filename):
-  filename = '/home/hope-yao/Documents/adversarial_defense/cifar/ckpt/crop_4_20_adv/half_half/lr_config1_adv/checkpoint-25001'
   FLAGS = tf.app.flags.FLAGS
   tfconfig = tf.ConfigProto(
       allow_soft_placement=True,
       log_device_placement=True,
   )
   tfconfig.gpu_options.allow_growth = True
-  with tf.Session(config=tfconfig) as sess:#
+  with tf.Session() as sess:#config=tfconfig
 
     # Restore the checkpoint
     saver.restore(sess, filename)
@@ -86,20 +93,18 @@ def evaluate_checkpoint(filename):
       dict_nat = {model.x_input: x_batch,
                   model.y_input: y_batch}
 
-      if 0:
-          x_batch_adv = attack.perturb(x_batch, y_batch, sess)
-      else:
-          x_batch_adv = get_PGD(sess, model.adv_grad, model.x_input, model.y_input, x_batch, y_batch, epsilon=8. / 255, a=2. / 255, k=7)
+      #x_batch_adv = attack.perturb(x_batch, y_batch, sess)
+      x_batch_adv = get_PGD(sess, model.adv_grad, dict_nat, model.x_input, epsilon=8. / 255, a=0.2 / 255, k=70)
 
       dict_adv = {model.x_input: x_batch_adv,
                   model.y_input: y_batch}
 
-      acc_i, cur_xent_nat = sess.run(
-                                      [model.accuracy,model.mean_xent],
+      nat_prediction, nat_voted_pred, acc_i, cur_xent_nat = sess.run(
+                                      [model.prediction, model.voted_pred, model.accuracy,model.mean_xent],
                                       feed_dict = dict_nat)
       cur_corr_nat = acc_i*config['eval_batch_size']
-      acc_i, cur_xent_adv = sess.run(
-                                      [model.accuracy,model.mean_xent],
+      adv_prediction, adv_voted_pred, acc_i, cur_xent_adv = sess.run(
+                                      [model.prediction, model.voted_pred, model.accuracy,model.mean_xent],
                                       feed_dict = dict_adv)
       cur_corr_adv = acc_i*config['eval_batch_size']
       print(eval_batch_size)
@@ -109,6 +114,39 @@ def evaluate_checkpoint(filename):
       total_xent_adv += cur_xent_adv
       total_corr_nat += cur_corr_nat
       total_corr_adv += cur_corr_adv
+
+      check_idx = ~ np.equal(nat_voted_pred, y_batch) * np.equal(adv_voted_pred, y_batch)
+      check_dict['nat_img'] += [x_batch[check_idx]]
+      check_dict['adv_img'] += [x_batch_adv[check_idx]]
+      check_dict['nat_pred'] += [nat_voted_pred[check_idx]]
+      check_dict['adv_pred'] += [adv_voted_pred[check_idx]]
+      check_dict['nat_preds'] += [nat_prediction[check_idx]]
+      check_dict['adv_preds'] += [adv_prediction[check_idx]]
+      check_dict['label'] += [y_batch[check_idx]]
+      check_dict['ckeck_idx'] += [check_idx]
+
+    check_dict['nat_img'] = np.concatenate(check_dict['nat_img'], 0)
+    check_dict['adv_img'] = np.concatenate(check_dict['adv_img'], 0)
+    check_dict['nat_pred'] = np.concatenate(check_dict['nat_pred'], 0)
+    check_dict['adv_pred'] = np.concatenate(check_dict['adv_pred'], 0)
+    check_dict['nat_preds'] = np.concatenate(check_dict['nat_preds'], 0)
+    check_dict['adv_preds'] = np.concatenate(check_dict['adv_preds'], 0)
+    check_dict['label'] = np.concatenate(check_dict['label'], 0)
+    check_dict['ckeck_idx'] = np.concatenate(check_dict['ckeck_idx'], 0)
+
+    if 1:
+        import matplotlib.pyplot as plt
+        jj = 20
+        plt.figure(figsize=(8.8, 2))
+        for idx in range(10):
+            plt.subplot(2, 10, idx + 1)
+            plt.imshow(check_dict['nat_img'][jj + idx])
+            plt.title('{}'.format(check_dict['nat_preds'][jj + idx]))
+            plt.axis('off')
+            plt.subplot(2, 10, idx + 1 + 10)
+            plt.imshow(check_dict['adv_img'][jj + idx])
+            plt.title('{}'.format(check_dict['adv_preds'][jj + idx]))
+            plt.axis('off')
 
     avg_xent_nat = total_xent_nat / num_eval_examples
     avg_xent_adv = total_xent_adv / num_eval_examples
@@ -129,35 +167,5 @@ def evaluate_checkpoint(filename):
     print('avg nat loss: {:.4f}'.format(avg_xent_nat))
     print('avg adv loss: {:.4f}'.format(avg_xent_adv))
 
-# Infinite eval loop
-while True:
-  cur_checkpoint = tf.train.latest_checkpoint(model_dir)
-
-  # Case 1: No checkpoint yet
-  if cur_checkpoint is None:
-    if not already_seen_state:
-      print('No checkpoint yet, waiting ...', end='')
-      already_seen_state = True
-    else:
-      print('.', end='')
-    sys.stdout.flush()
-    time.sleep(10)
-  # Case 2: Previously unseen checkpoint
-  elif cur_checkpoint != last_checkpoint_filename:
-    print('\nCheckpoint {}, evaluating ...   ({})'.format(cur_checkpoint,
-                                                          datetime.now()))
-    sys.stdout.flush()
-    last_checkpoint_filename = cur_checkpoint
-    already_seen_state = False
-    evaluate_checkpoint(cur_checkpoint)
-  # Case 3: Previously evaluated checkpoint
-  else:
-    if not already_seen_state:
-      print('Waiting for the next checkpoint ...   ({})   '.format(
-            datetime.now()),
-            end='')
-      already_seen_state = True
-    else:
-      print('.', end='')
-    sys.stdout.flush()
-    time.sleep(10)
+if __name__ == '__main__':
+    evaluate_checkpoint('/home/hope-yao/Documents/adversarial_defense/cifar/ckpt/crop_4_20_adv/half_half/lr_config1_adv/checkpoint-25001')

cifar/script/model_cifar.py
@@ -37,7 +37,7 @@ class Model(object):
     xent = []
     prediction = []
     tower_grads = []
-    adv_grad = []
+    self.adv_grads = []
     
     with tf.variable_scope(tf.get_variable_scope()) as vscope:
         for ii in xrange(len(self.loc)):
@@ -63,8 +63,8 @@ class Model(object):
                 prediction += [crop_prediction]
 
                 # adversarial gradient per mini-batch
-                adv_grad_i = tf.gradients(crop_xent, self.x_input)[0]
-                adv_grad += [adv_grad_i]
+                adv_grad_i = tf.gradients(tf.reduce_sum(crop_xent), self.x_input)[0]
+                self.adv_grads += [adv_grad_i]
 
     self.xent = tf.stack(xent, 1)
     self.mean_xent = tf.reduce_mean(self.xent)
@@ -75,7 +75,7 @@ class Model(object):
     update_network_op =  self.opts.apply_gradients(self.grads,global_step=self.global_step)
     self.train_step = tf.group(update_network_op, update_batchnorm_op)
 
-    self.adv_grad = tf.reduce_mean(adv_grad,0)
+    self.adv_grad = tf.reduce_sum(self.adv_grads,0)
     self.voted_pred = []
     batch_size = config['training_batch_size'] if mode == "train" else config['eval_batch_size']
     for i in range(batch_size) :  # loop over a batch

cifar/script/pgd_attack.py
@@ -21,7 +21,7 @@ class LinfPGDAttack:
     self.epsilon = epsilon
     self.num_steps = num_steps
     self.step_size = step_size
-    self.rand = random_start
+    self.rand = False
 
     if loss_func == 'xent':
       loss = model.xent
@@ -38,14 +38,15 @@ class LinfPGDAttack:
       print('Unknown loss function. Defaulting to cross-entropy')
       loss = model.xent
 
-    self.grad = tf.gradients(loss, model.x_input)[0]
+    self.grads = [tf.gradients(tf.reduce_mean(model.xent[:, i]), model.x_input)[0] for i in range(4)]
+    self.grad = tf.gradients(tf.reduce_sum(loss), model.x_input)[0]
 
   def perturb(self, x_nat, y, sess):
     """Given a set of examples (x_nat, y), returns a set of adversarial
        examples within epsilon of x_nat in l_infinity norm."""
     if self.rand:
       x = x_nat + np.random.uniform(-self.epsilon, self.epsilon, x_nat.shape)
-      x = np.clip(x, 0, 255) # ensure valid pixel range
+      x = np.clip(x, 0, 1) # ensure valid pixel range
     else:
       x = np.copy(x_nat)
 
@@ -56,7 +57,7 @@ class LinfPGDAttack:
       x = np.add(x, self.step_size * np.sign(grad), out=x, casting='unsafe')
 
       x = np.clip(x, x_nat - self.epsilon, x_nat + self.epsilon)
-      x = np.clip(x, 0, 255) # ensure valid pixel range
+      x = np.clip(x, 0, 1) # ensure valid pixel range
 
     return x
 

cifar/script/pgd_multiGPU.py
@@ -11,30 +11,31 @@ import tensorflow as tf
 import numpy as np
 import copy
 
-def get_PGD(sess, adv_grad, feed_dict_pgd, x_input_pl, epsilon=0.1, a=0.002, k=50, rand=True, dist='Linf'):
+def get_PGD(sess, adv_grad, x_input_pl, y_input_pl, x_nat, y_nat, epsilon=0.1, a=0.002, k=50, rand=False, dist='Linf'):
   if dist == 'Linf':
-    x = get_PGD_Linf(sess, adv_grad, feed_dict_pgd, x_input_pl, epsilon, a, k, rand)
+    x = get_PGD_Linf(sess, adv_grad, x_input_pl, y_input_pl, x_nat, y_nat, epsilon, a, k, rand)
   elif dist == 'L2':
-    x = get_PGD_L2(sess, adv_grad, feed_dict_pgd, x_input_pl, epsilon, a, k, rand)
+    x = get_PGD_L2(sess, adv_grad, x_input_pl, y_input_pl, x_nat, y_nat, epsilon, a, k, rand)
   else:
     print('not implemented')
   return x
 
 
-def get_PGD_Linf(sess, adv_grad, feed_dict_pgd, x_input_pl, epsilon, a, k, rand):
+def get_PGD_Linf(sess, adv_grad, x_input_pl, y_input_pl, x_nat, y_nat, epsilon, a, k, rand):
   """Given a set of examples (x_nat, y), returns a set of adversarial
      examples within epsilon of x_nat in l_infinity norm."""
 
-  x_nat = feed_dict_pgd[x_input_pl]
   if rand:
-    x = x_nat + np.random.uniform(-epsilon, epsilon, x_nat.shape)
+    x = np.copy(x_nat) + np.random.uniform(-epsilon, epsilon, x_nat.shape)
   else:
     x = np.copy(x_nat)
 
+
   for i in range(k):
-    grad = sess.run(adv_grad, feed_dict=feed_dict_pgd)
+    grad = sess.run(adv_grad, feed_dict={x_input_pl: x, y_input_pl: y_nat})
     #print(grad.shape)
-    x += a * np.sign(grad)
+    # x += a * np.sign(grad)
+    x = np.add(x, a * np.sign(grad), out=x, casting='unsafe')
     x = np.clip(x, x_nat - epsilon, x_nat + epsilon)
     x = np.clip(x, 0, 1)  # ensure valid pixel range
 
@@ -60,11 +61,10 @@ def sphere_rand(input_size, epsilon):
   return  np.concatenate(x,0)
 
 
-def get_PGD_L2(sess, adv_grad, feed_dict_pgd, x_input_pl, epsilon, a, k, rand):
+def get_PGD_L2(sess, adv_grad, x_input_pl, y_input_pl, x_nat, y_nat, epsilon, a, k, rand):
   """Given a set of examples (x_nat, y), returns a set of adversarial
      examples within epsilon of x_nat in l_infinity norm."""
 
-  x_nat = feed_dict_pgd[x_input_pl]
   input_size = x_input_pl.get_shape().as_list()
   bs = input_size[0]
 
@@ -76,7 +76,7 @@ def get_PGD_L2(sess, adv_grad, feed_dict_pgd, x_input_pl, epsilon, a, k, rand):
     x = np.copy(x_nat)
 
   for i in range(k):
-    grad = sess.run(adv_grad, feed_dict=feed_dict_pgd)
+    grad = sess.run(adv_grad, feed_dict={x_input_pl: x, y_input_pl: y_nat})
 
     if 1:
       # attack normalize

cifar/script/train_cifar.py
@@ -94,12 +94,15 @@ with tf.Session() as sess:#config=tfconfig
 
     nat_dict = {model.x_input: x_batch.reshape(batch_size//2, 32, 32, 3),
                       model.y_input: y_batch}
-    x_batch_adv = get_PGD(sess, model.adv_grad, nat_dict, model.x_input, epsilon=8. / 255, a=2. / 255, k=7)
-    x_batch_adv_near = get_PGD(sess, model.adv_grad, nat_dict, model.x_input, epsilon=1. / 255, a=0.5 / 255, k=3)
-    adv_dict = {model.x_input: np.concatenate([x_batch_adv,x_batch_adv_near],0).reshape(batch_size, 32, 32, 3),
-                      model.y_input: np.concatenate([y_batch,y_batch],0)}
-    nat_dict = {model.x_input: np.concatenate([x_batch,x_batch],0).reshape(batch_size, 32, 32, 3),
-                      model.y_input: np.concatenate([y_batch,y_batch],0)}
+    x_batch_adv = get_PGD(sess, model.adv_grad, model.x_input, model.y_input, x_batch, y_batch, epsilon=8. / 255, a=2. / 255, k=7)
+    adv_dict = {model.x_input: x_batch_adv.reshape(batch_size//2, 32, 32, 3),
+                      model.y_input: y_batch}
+    # x_batch_adv = get_PGD(sess, model.adv_grad, nat_dict, model.x_input, epsilon=8. / 255, a=2. / 255, k=7)
+    # x_batch_adv_near = get_PGD(sess, model.adv_grad, nat_dict, model.x_input, epsilon=1. / 255, a=0.5 / 255, k=3)
+    # adv_dict = {model.x_input: np.concatenate([x_batch_adv,x_batch_adv_near],0).reshape(batch_size, 32, 32, 3),
+    #                   model.y_input: np.concatenate([y_batch,y_batch],0)}
+    # nat_dict = {model.x_input: np.concatenate([x_batch,x_batch],0).reshape(batch_size, 32, 32, 3),
+    #                   model.y_input: np.concatenate([y_batch,y_batch],0)}
     _, adv_acc_i, adv_xent_i = sess.run([model.train_step, model.accuracy, model.xent], feed_dict=adv_dict)
     nat_acc_i, nat_xent_i = sess.run([model.accuracy, model.xent], feed_dict=nat_dict)
     nat_acc += [nat_acc_i]