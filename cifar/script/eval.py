fixed a bug in PGD multi-gpu, added some gradient masking checking code

cifar/script/eval.py
@@ -25,7 +25,6 @@ from pgd_multiGPU import *
 # Global constants
 with open('config.json') as config_file:
   config = json.load(config_file)
-num_eval_examples = config['num_eval_examples']
 eval_batch_size = config['eval_batch_size']
 eval_on_cpu = config['eval_on_cpu']
 data_path = config['data_path']
@@ -52,10 +51,12 @@ last_checkpoint_filename = ''
 already_seen_state = False
 
 saver = tf.train.Saver()
-summary_writer = tf.summary.FileWriter(eval_dir)
+summary_writer_eval = tf.summary.FileWriter(eval_dir)
+summary_writer_train = tf.summary.FileWriter(model_dir)
+input_data_eval = cifar.eval_data
+input_data_train = cifar.train_data
 
-# A function for evaluating a single checkpoint
-def evaluate_checkpoint(filename):
+def evaluate_checkpoint(filename, input_data, summary_writer):
   #filename = '/home/hope-yao/Documents/adversarial_defense/cifar/ckpt/crop_4_20_adv/half_half/lr_config1_adv/checkpoint-25001'
   FLAGS = tf.app.flags.FLAGS
   tfconfig = tf.ConfigProto(
@@ -67,7 +68,7 @@ def evaluate_checkpoint(filename):
 
     # Restore the checkpoint
     saver.restore(sess, filename)
-
+    num_eval_examples = input_data.ys.shape[0]
     # Iterate over the samples batch-by-batch
     num_batches = int(math.ceil(num_eval_examples / eval_batch_size))
     total_xent_nat = 0.
@@ -79,9 +80,9 @@ def evaluate_checkpoint(filename):
       bstart = ibatch * eval_batch_size
       bend = min(bstart + eval_batch_size, num_eval_examples)
 
-      x_batch = cifar.eval_data.xs[bstart:bend, :]
+      x_batch = input_data.xs[bstart:bend, :]
       x_batch = np.asarray(x_batch, 'float32') / 255.
-      y_batch = cifar.eval_data.ys[bstart:bend]
+      y_batch = input_data.ys[bstart:bend]
 
       dict_nat = {model.x_input: x_batch,
                   model.y_input: y_batch}
@@ -116,10 +117,8 @@ def evaluate_checkpoint(filename):
     acc_adv = total_corr_adv / num_eval_examples
 
     summary = tf.Summary(value=[
-          tf.Summary.Value(tag='xent adv eval', simple_value= avg_xent_adv),
           tf.Summary.Value(tag='xent adv', simple_value= avg_xent_adv),
           tf.Summary.Value(tag='xent nat', simple_value= avg_xent_nat),
-          tf.Summary.Value(tag='accuracy adv eval', simple_value= acc_adv),
           tf.Summary.Value(tag='accuracy adv', simple_value= acc_adv),
           tf.Summary.Value(tag='accuracy nat', simple_value= acc_nat)])
     summary_writer.add_summary(summary, global_step.eval(sess))
@@ -149,7 +148,8 @@ while True:
     sys.stdout.flush()
     last_checkpoint_filename = cur_checkpoint
     already_seen_state = False
-    evaluate_checkpoint(cur_checkpoint)
+    evaluate_checkpoint(cur_checkpoint, input_data_train, summary_writer_train)
+    evaluate_checkpoint(cur_checkpoint, input_data_eval, summary_writer_eval)
   # Case 3: Previously evaluated checkpoint
   else:
     if not already_seen_state:

cifar/script/model_cifar.py
@@ -21,7 +21,9 @@ class Model(object):
     self.y_input = tf.placeholder(tf.int64, shape=None)
     self.config = config
     self.y_pred = []
-    self.loc = [[14, 14], [14, 18], [18, 14], [18, 18]]
+    # self.loc = [[14, 14], [14, 18], [18, 14], [18, 18]]
+    # self.loc = [[12, 12], [12, 20], [20, 12], [20, 20]]
+    self.loc = [[12, 12], [12, 16], [12, 20], [16, 12], [16, 20], [20, 12], [20, 16], [20, 20]]
 
     # Setting up the optimizer
     step_size_schedule = config['step_size_schedule']
@@ -45,7 +47,7 @@ class Model(object):
             with tf.device('/gpu:%d' % gpu_i):
 
                 loc_x, loc_y = self.loc[ii]
-                x_crop_i = self.x_input[:, loc_x - 14:loc_x + 14, loc_y - 14:loc_y + 14, :]
+                x_crop_i = self.x_input[:, loc_x - 12:loc_x + 12, loc_y - 12:loc_y + 12, :]
                 pre_softmax = self._build_model(x_crop_i)
 
                 # reuse variables

cifar/script/train_cifar.py
@@ -29,7 +29,7 @@ np.random.seed(config['np_random_seed'])
 # Setting up training parameters
 max_num_training_steps = config['max_num_training_steps']
 num_output_steps = config['num_output_steps']
-num_summary_steps = config['num_summary_steps']
+# num_summary_steps = config['num_summary_steps']
 num_checkpoint_steps = config['num_checkpoint_steps']
 data_path = config['data_path']
 batch_size = config['training_batch_size']
@@ -59,14 +59,6 @@ if not os.path.exists(model_dir):
 # - eval of different runs
 
 saver = tf.train.Saver(max_to_keep=30)
-tf.summary.scalar('accuracy nat train', model.accuracy)
-tf.summary.scalar('xent nat train', model.mean_xent)
-tf.summary.image('images nat train', model.x_input)
-merged_summaries_nat = tf.summary.merge_all()
-tf.summary.scalar('accuracy adv train', model.accuracy)
-tf.summary.scalar('xent adv train', model.mean_xent)
-tf.summary.image('images adv train', model.x_input)
-merged_summaries_adv = tf.summary.merge_all()
 
 # keep the configuration file with the model for reproducibility
 shutil.copy('config.json', model_dir)
@@ -83,7 +75,6 @@ with tf.Session() as sess:#config=tfconfig
   cifar = cifar10_input.AugmentedCIFAR10Data(raw_cifar, sess, model)
 
   # Initialize the summary writer, global variables, and our time counter.
-  summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
   sess.run(tf.global_variables_initializer())
   training_time = 0.0
 
@@ -120,12 +111,13 @@ with tf.Session() as sess:#config=tfconfig
         print('    training adv accuracy {:.4}%'.format(np.mean(adv_acc) * 100))
         print('    {} samples per second'.format(num_output_steps * batch_size / training_time))
         training_time = 0.0
-    # Tensorboard summaries
-    if ii % num_summary_steps == 0:
-      summary = sess.run(merged_summaries_adv, feed_dict=adv_dict)
-      summary_writer.add_summary(summary, model.global_step.eval(sess))
-      summary = sess.run(merged_summaries_nat, feed_dict=nat_dict)
-      summary_writer.add_summary(summary, model.global_step.eval(sess))
+
+    # # Tensorboard summaries
+    # if ii % num_summary_steps == 0:
+    #   summary = sess.run(merged_summaries_adv, feed_dict=adv_dict)
+    #   summary_writer.add_summary(summary, model.global_step.eval(sess))
+    #   summary = sess.run(merged_summaries_nat, feed_dict=nat_dict)
+    #   summary_writer.add_summary(summary, model.global_step.eval(sess))
 
 
     # Write a checkpoint